6f82a03cccb043d12a2bfd29ad382770d3cb3cf5
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-04-04 12:54:45 -0400

    Incruindo a rota producao

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstoqueController;
+use App\Http\Controllers\ProducaoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +24,6 @@ Route::get("ingredientes/getAll", [EstoqueController::class, 'getAll']);
 Route::get("ingrediente/getId/{id}", [EstoqueController::class, 'getId']);
 Route::put("ingrediente/atualizar/{id}", [EstoqueController::class, 'atualizar']);
 Route::delete("ingrediente/deletar/{id}", [EstoqueController::class, 'deletar']);
+
+Route::post("producao/cadastrar", [ProducaoController::class, 'cadastrar']);
+Route::get("producao/getAll", [ProducaoController::class, 'getAll']);
